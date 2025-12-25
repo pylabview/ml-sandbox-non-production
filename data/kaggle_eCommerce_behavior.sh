38fa935247c999ab19735b492254d35dfd656ec2
Rodrigo Arguello Serrano <rodrigo19662001@gmail.com>
2025-12-25 09:50:38 -0800

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   data/kaggle_eCommerce_behavior.sh

---
+++ data/kaggle_eCommerce_behavior.sh
@@ -0,0 +1,35 @@
+#!/bin/zsh
+
+wget https://data.rees46.com/datasets/marketplace/2019-Oct.csv.gz;
+
+sleep 5;
+
+wget https://data.rees46.com/datasets/marketplace/2019-Nov.csv.gz;
+
+sleep 5;
+
+wget https://data.rees46.com/datasets/marketplace/2019-Dec.csv.gz;
+
+sleep 5;
+
+wget https://data.rees46.com/datasets/marketplace/2019-Dec.csv.gz;
+
+sleep 5;
+
+wget https://data.rees46.com/datasets/marketplace/2020-Jan.csv.gz;
+
+sleep 5;
+
+wget https://data.rees46.com/datasets/marketplace/2020-Feb.csv.gz;
+
+sleep 5;
+
+wget https://data.rees46.com/datasets/marketplace/2020-Mar.csv.gz;
+
+sleep 5;
+
+wget https://data.rees46.com/datasets/marketplace/2020-Apr.csv.gz;
+
+sleep 5;
+
+gunzip *.gz
